83cfd8cfe055f4dc1031c1d6140b6ef6577e7f4d
Agus <ag.rifai12@gmail.com>
2020-10-22 20:06:21 +0700

    Adding edit Book

---
+++ routes/books/editBook.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express.Router()
+const db = require('../../controller/dbController')
+
+app.patch('/books', (req, res) => {
+    const body = req.body
+    const id = req.query.id
+    const result = db.edit('books', id, body)
+    if (result) {
+        res.send(result)
+    }
+    else {
+        res.status(404).send('Not Found')
+    }
+})
+
+
+module.exports = app
