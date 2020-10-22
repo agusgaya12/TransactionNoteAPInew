0e9ca31bdb74788e329782884b003d38b65555df
Agus <ag.rifai12@gmail.com>
2020-10-22 20:08:05 +0700

    Adding edit Stores

---
+++ routes/stores/editStores.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express.Router()
+const db = require('../../controller/dbController')
+
+app.patch('/stores', (req, res) => {
+    const body = req.body
+    const id = req.query.id
+    const result = db.edit('stores', id, body)
+    if (result) {
+        res.send(result)
+    }
+    else {
+        res.status(404).send('Not Found')
+    }
+})
+
+
+module.exports = app
